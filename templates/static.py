da0187949194dc98e7ece1b36130187b8ccad5c7
ohaddan <ohad.dan@gmail.com>
2018-11-11 10:29:38 +0200

    Fixing the order of alternatives

--- templates/static.py
+++ templates/static.py
@@ -1,10 +1,10 @@
 # This is a template for a static mechanism. All lines with a "#" prefix
-# are an explanation for the template and *not* be part of the submitted
+# are an explanation for the template and *not* part of the submitted
 # reward schedule.
 # The rewards for both alternatives should be listed in square brackets
 # ("[]") and separated by commas. Each entry in the list of rewards should
 # be either '1' (indicating a reward) or '0' (indicating no reward).
-# The first line details the rewards allocation to the "target" side,
+# The first line details the reward allocation to the "target" side,
 # the side in which the reward-schedule should maximize the number of choices.
 # The second line describes the rewards in the "anti-target" side in which
 # the number of choices should be minimized. Note that each list should include
@@ -13,8 +13,8 @@
 # (i.e. the "=" sign, the order of the lines and the text preceding it should not
 # be modified).
 # In the following example, the target alternative is assigned rewards in the
-# first 25 trials (and no rewards in all future trials) whereas the allocation
-# of rewards to the anti-target alternative starts with 75 trials of no reward
-# allocation, followed by 25 trials of rewards.
-bias_target = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-anti_bias = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# first 25 trials (and no rewards in all consecutive  future trials) whereas the allocation
+# of rewards to the anti-target alternative starts with 75 trials of no reward,
+# followed by 25 trials of rewards.
+bias_target = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+anti_bias = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]

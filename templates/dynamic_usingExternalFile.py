a10e6fd0805f7938abe22006c15dd460eba5f87c
ohaddan <ohad.dan@gmail.com>
2018-11-11 14:09:01 +0200

    Update instructions

--- templates/dynamic_usingExternalFile.py
+++ templates/dynamic_usingExternalFile.py
@@ -1,64 +1,73 @@
 # This file provides an explanation and example usage of a dynamic allocation
 # file for the bias induction competition. For more details on the competition,
-#  see [here](http://decision-making-lab.com/competition/index.html)
+# see [here](http://decision-making-lab.com/competition/index.html)
 
 ###############################################################################
 # What is a dynamic allocation model?
 ###############################################################################
 # A dynamic allocation model is used to determine the rewards in a single trial
-#  of an experiment run as part of the bias induction competition. This output
-#  should indicate the allocation of rewards for the two alternatives used in
-#  the experiment in a specific trial. The rewards available for allocation are
-#  binary (either 1 or 0) and are constrained such that during the 100 trials
-#  of the experiment, each of the alternatives should be allocated exactly 25
-#  rewards (i.e. 1's, and 75 should be allocated with 0's). The input of the
-#  dynamic allocation model is current experiment's history, namely previous
-#  allocations and their respective choices.
+#  for a single subject, participating in the experiment of The Choice  
+#  Engineering Competition. The allocation mechanism should indicate whether or 
+#  not to allocate  a reward for either of the experiment’s two alternatives.
+#  The rewards available for allocation are binary (either 1 or 0) and are 
+#  constrained such that during the 100 trials of the experiment, each of the
+#  alternatives should be associated with exactly 25 rewards (i.e. 1's, and
+#  75 should be associated with 0's). The input to the dynamic allocation model
+#  is current experiment's history, namely previous allocations and respective
+#  choices, and its output is allocation of rewards for the next trial.
 #
-# The goal of the competition, and the dynamic allocation model, is to design
-# an allocation mechanism that would maximize the choices in one specific
-# alternative, termed the "target alternative". In the experiment, the target
-# alternative will be placed randomly either on the left or the right part of
-# the participant's screen. However, in the output of the allocation model, the
-#  target alternative should be always placed first.
+# The goal of the competition, and hence of the dynamic allocation model, is to 
+#  generate an allocation mechanism that would maximize the choices of humans
+#  in one specific alternative, termed the "target alternative". In the 
+#  competition’s experiment, the target alternative will be placed randomly 
+#  either on the left or the right part of the participant's screen. However,
+#  in the output of the allocation model, the target alternative should be
+#  always placed first (see below).
 
 ###############################################################################
-# How should a dynamic allocation model file be used
+# How should a dynamic allocation file be used
 ###############################################################################
 # In the course of an experiment, your dynamic allocation model would be called
-#  repeatedly, once for every trial, and the allocation provided by the
-#  will be revealed to the participant, according to her choice.
+#  repeatedly, once for every trial, and the allocation indicated by your  
+#  algorithm will be revealed to the participant. Note that the paradigm used
+#  in the competition is that of “partial feedback”, by which only the reward 
+#  associated with the chosen alternative is revealed (the reward associated 
+#  with the unchosen alternative is not). Hence, only one of the rewards output 
+#  by the dynamic allocation mechanism is actually revealed to the subject, 
+#  according to her choice.
 #
-# A. Receiving input: your model will be called with two command-line arguments
-#  that you may parse
+# A. Receiving input: your model will be called with three command-line
+#  arguments that you may parse
 # e.g. with [sys.argv](https://docs.python.org/3.7/library/sys.html#sys.argv)).
 #     1. The first input is a list of previous allocations to the target
 #      alternative. Each entry in the list is either 1, indicating that in that
 #      index a reward was allocated, or 0, indicating that it was not. For
 #      example, the list [1, 0, 0, 0] received as the first input for your
-#      model indicates that the experiment is currently at its 5'th trial,
+#      program indicates that the experiment is currently at its 5'th trial,
 #      that on the first trial a reward was allocated to the target alternative
-#      and that no rewards were allocated in trial 2, 3, and 4.
-#     2. The second input is in the same format as the first, but indicating
+#      and that no rewards were allocated in trials 2, 3, and 4.
+#     2. The second input is in the same format as the first, but it indicates 
 #      previous rewards to the second ("anti-target") alternative. Hence, for
-#      example, the list [0, 1, 1] as the second input to your model indicates
-#      that it is currently the 4'th trial, that on the first trial rewards
-#      were not allocated to the anti-target alternative and that on the second
-#      and third trial rewards were allocated to that alternative.
+#      example, the list [0, 1, 1] as the second input to your program indicates
+#      that the game is currently at its 4'th trial, that on the first trial a 
+#      reward was not allocated to the anti-target alternative and that on the 
+#      second and third trial rewards were allocated to that alternative.
 #     3. The third input is a list of previous choices, where 1's indicate a
 #      choice in the target alternative and 0's indicate choice in the
 #      anti-target alternative. For example, the list [1, 1, 1, 0, 0, 0]
-#      received as the third input indicates that it is currently the 7'th
-#      trial, that on the first three trials the target side was chosen by the
-#      user and that on the last three trials it was not.
+#      received as the third input indicates that the experiment is currently at  
+#      its 7'th trial, that on the first three trials the target side was chosen
+#      by the subject and that on the last three trials it was not.
 # B. Providing output - Your model should indicate the allocation of rewards
-# by printing (to the standard sys.stdout, e.g. using print) a single string in
-#  the format of "(T, N)", where both T and N are either the character 1 or 0,
+#  by printing (to the standard sys.stdout, e.g. using 
+#  [print]( https://docs.python.org/3/tutorial/inputoutput.html)) a single string
+#  in the format of "(T, N)", where both T and N are either the character 1 or 0,
 #  T represents the allocation to the target side and N the allocation the
 #  non-target side.
-# Hence, your model output should be one of the following four strings
-# "(0, 0)",  "(0, 1)",  "(1, 0)",  "(1, 1)". To prevent formatting issues,
-# you may use the "output" function provided in this file.
+#  Hence, your model output should be one of the following four strings
+#  "(0, 0)",  "(0, 1)",  "(1, 0)",  "(1, 1)". To prevent formatting issues,
+#  you may use the "output" function provided in this file.
+
 
 ###############################################################################
 # Code start

1e8a0c071c1ff7b863a12ff32b7b7ac885a622e1
Ohad Dan <ohad.dan@gmail.com>
2024-12-23 11:47:14 -0500

    Moving the analysis to a different repository

--- competition_analysis/CATIE/CATIE_implementation_helpers/getExploreProb.m
+++
@@ -1,8 +0,0 @@
-function [ pExp ] = getExploreProb( epsilon, currentSurp, meanSurp )
-%Sets probability of exploration
-%   Detailed explanation goes here
-
-pExp = epsilon* (1+ currentSurp + meanSurp) /3;
-
-end
-
--- competition_analysis/helper_functions/sem.m
+++
@@ -1,6 +0,0 @@
-function standard_error_of_the_mean = sem(v)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-standard_error_of_the_mean = std(v)./sqrt(length(v)-1);
-end
-
